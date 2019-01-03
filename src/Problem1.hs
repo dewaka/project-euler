1bf5a6997ff3a5b4fa0f58df7500eb22a30791a4
Chathura Colombage <dcdewaka@gmail.com>
2019-01-03 21:08:07 +0000

    problem 1 - faster solution + test

--- src/Problem1.hs
+++ src/Problem1.hs
@@ -4,13 +4,34 @@
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
 module Problem1 (
-  solution_slow
+  solutionSlow,
+  solutionFast,
+  solutionTests
 ) where
 
+import Test.HUnit
+
+-- Brute force solution
 multiplesOf :: [Int] -> [Int]
 multiplesOf ds = filter (hasFactors ds) [1..]
   where
     hasFactors ds num = or $ map (\t -> num `rem` t == 0) ds
 
-solution_slow :: Int
-solution_slow = sum $ takeWhile (<1000) $ multiplesOf [3, 5]
+solutionSlow :: Int
+solutionSlow = sum $ takeWhile (<1000) $ multiplesOf [3, 5]
+
+-- Faster solution based on geometric sum
+sumDivisibleBy :: Int -> Int -> Int
+sumDivisibleBy n lim = (n * p * (p + 1)) `div` 2
+  where
+    p = lim `div` n
+
+solutionFast :: Int
+solutionFast = sumDivisibleBy 3 999 + sumDivisibleBy 5 999 - sumDivisibleBy 15 999
+
+-- Tests
+test1 = TestCase $ assertEqual "slow and fast solutions agree" solutionFast solutionSlow
+
+solutionTests =
+  runTestTT $ TestList [ test1 ]
+
